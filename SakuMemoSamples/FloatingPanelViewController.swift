cb2f6b0e5dc17b84976ded74658edd633fb5a2e4
CatLuck2 <freeaddress834@gmail.com>
2021-12-30 20:30:23 +0900

    FloatingPanelでハーフモーダルを表示

---
+++ SakuMemoSamples/FloatingPanelViewController.swift
@@ -0,0 +1,12 @@
+//
+//  FloatingPanelViewController.swift
+//  SakuMemoSamples
+//
+//  Created by 藤澤洋佑 on 2021/12/30.
+//
+
+import UIKit
+
+class FloatingPanelViewController: UIViewController {
+    
+}
--- SakuMemoSamples/ViewController.swift
+++ SakuMemoSamples/ViewController.swift
@@ -8,13 +8,23 @@
 import UIKit
 import FloatingPanel
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FloatingPanelControllerDelegate {
+    
+    private var fpc = FloatingPanelController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        fpc.delegate = self
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let halfModalViewController = segue.destination
+    
+    @IBAction func presentHalfModalButton(_ sender: UIButton) {
+        guard let destinationVC = self.storyboard?.instantiateViewController(withIdentifier: "fpc") as? FloatingPanelViewController else { return }
+        self.presentFloatingPanel(nextVC: destinationVC)
+    }
+    
+    /// sheetPresentationControllerでハーフモーダルを表示
+    func presentSheetPresentationController(destinationVC: UIViewController) {
+        let halfModalViewController = destinationVC
         if let halfModalVC = halfModalViewController.sheetPresentationController {
             halfModalVC.detents = [.medium(), .large()]
             halfModalVC.prefersScrollingExpandsWhenScrolledToEdge = false
@@ -22,5 +32,11 @@ class ViewController: UIViewController {
             halfModalVC.preferredCornerRadius = 12.0
             halfModalVC.largestUndimmedDetentIdentifier = .medium
         }
+    }
+    
+    /// FloatingPanelでハーフモーダルを表示
+    func presentFloatingPanel(nextVC: UIViewController) {
+        fpc.set(contentViewController: nextVC)
+        fpc.addPanel(toParent: self)
     }
 }
