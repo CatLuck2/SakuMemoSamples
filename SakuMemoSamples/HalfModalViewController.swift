a052e4fcbe5f1a03f80af61ca4222ca91a39f51a
CatLuck2 <freeaddress834@gmail.com>
2021-12-30 16:08:52 +0900

    ハーフモーダルを実装 （sheetPresentationController）

---
+++ SakuMemoSamples/HalfModalViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HalfModalViewController.swift
+//  SakuMemoSamples
+//
+//  Created by 藤澤洋佑 on 2021/12/30.
+//
+
+import UIKit
+
+class HalfModalViewController: UIViewController {
+    override func viewDidLoad() {
+    }
+}
---
+++ SakuMemoSamples/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SakuMemoSamples
+//
+//  Created by 藤澤洋佑 on 2021/12/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let halfModalViewController = segue.destination
+        if let halfModalVC = halfModalViewController.sheetPresentationController {
+            halfModalVC.detents = [.medium(), .large()]
+            halfModalVC.prefersScrollingExpandsWhenScrolledToEdge = false
+            halfModalVC.prefersGrabberVisible = true
+            halfModalVC.preferredCornerRadius = 12.0
+            halfModalVC.largestUndimmedDetentIdentifier = .medium
+        }
+    }
+
+}
+
