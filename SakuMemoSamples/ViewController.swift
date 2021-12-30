99240342166d121491f2355e91a2f9ae3d575b65
CatLuck2 <freeaddress834@gmail.com>
2021-12-30 21:15:03 +0900

    FloatingPanel上に文字装飾用のボタンを設置

--- SakuMemoSamples/ViewController.swift
+++ SakuMemoSamples/ViewController.swift
@@ -14,7 +14,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        fpc.delegate = self
     }
     
     @IBAction func presentHalfModalButton(_ sender: UIButton) {
