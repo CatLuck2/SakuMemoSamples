6b0fce6231b213bc266e3a4b1a3519588014e2f2
CatLuck2 <freeaddress834@gmail.com>
2022-01-02 14:40:36 +0900

    AttributedTextの検証用として、ViewControllerとUI部品を配置

---
+++ SakuMemoSamples/AttributedStringViewController.swift
@@ -0,0 +1,55 @@
+//
+//  AttributedStringViewController.swift
+//  SakuMemoSamples
+//
+//  Created by 藤澤洋佑 on 2022/01/02.
+//
+
+import UIKit
+
+class AttributedStringViewController: UIViewController {
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        
+    }
+    
+    @IBAction func headlineFirstButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func headlineSecondButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func headlineThirdButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func plainTextButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func boldButton(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func italicButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func underlineButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func strikeThroughButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func bulletedListButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func numberedListButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func checkListButton(_ sender: UIButton) {
+    }
+    
+    
+}
+
